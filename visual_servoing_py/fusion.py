4fbed49b83c3001883f6700bc7de622b8c38f415
Victor BELLOT <victor.bellot@ensta-bretagne.org>
2023-11-23 11:38:11 +0100

    Update fusion.py

--- visual_servoing_py/fusion.py
+++ visual_servoing_py/fusion.py
@@ -12,8 +12,11 @@ def detect_ball(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Define the lower and upper HSV range for yellow
-    lower_yellow = np.array([15, 100, 32])  # sim : 20 100 100
-    upper_yellow = np.array([27, 255, 255])  # sim : 30 255 255
+    lower_yellow = np.array([20, 100, 100])  # sim
+    upper_yellow = np.array([30, 255, 255])  # sim
+
+    # lower_yellow = np.array([15, 100, 32])  # real
+    # upper_yellow = np.array([27, 255, 255])  # real
 
     # Create a mask to isolate yellow regions
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -30,8 +33,10 @@ def detect_ball(image):
         ball_contour = max(contours, key=lambda contour: cv2.contourArea(contour))
 
         perimeter = cv2.arcLength(ball_contour, True)
-        print("Radius estimate:", perimeter / (2 * np.pi))
-        print("Area estimate:", np.sqrt(cv2.contourArea(ball_contour)))
+        
+        # Ball radius estimate
+        print("Radius estimate from perimeter:", perimeter / (2 * np.pi))
+        print("Radius estimate from area:", np.sqrt(cv2.contourArea(ball_contour) / np.pi))
         
         return True, np.mean(ball_contour[:, :, 0], dtype=int), np.mean(ball_contour[:, :, 1], dtype=int)
     else:
@@ -56,8 +61,13 @@ if __name__ == '__main__':
     fps = 10.
     dt_loop = 1. / fps
 
-    fractionMaxSpeed = 0.1
-    proportionalConstant = dt_loop * np.pi / 256  # such as deltaAngle = pixelError * proportionalConstant
+    head_fraction_max_speed = 0.1
+    head_proportional_constant = dt_loop * np.pi / 256  # such as deltaAngle = pixelError * proportionalConstant
+
+    body_proportional_constant = 1.0
+    body_step_frequency = 1.0  # maximum step frequency
+    body_head_min_error = np.pi / 6  # in radians
+
     names = ["HeadYaw", "HeadPitch"]
 
     if len(sys.argv) == 3:
@@ -109,6 +119,16 @@ if __name__ == '__main__':
     while True:
         t0_loop = time.time()
 
+        # tends to align body with head
+        yaw_head, _ = motionProxy.getAngles(names, True)  # assuming between -pi and +pi
+
+        dx_factor = 0.0
+        dy_factor = 0.0
+        d_yaw_factor = max(min(body_proportional_constant * yaw_head, 1), -1) if abs(yaw_head) > body_head_min_error else 0.0
+
+        motionProxy.moveToward(dx_factor, dy_factor, d_yaw_factor, [["Frequency", body_step_frequency]])
+
+        # get image and detect ball
         img_ok, img, nx, ny = nao_drv.get_image()
         ball_detected, bx, by = detect_ball(img)
 
@@ -116,17 +136,11 @@ if __name__ == '__main__':
         if ball_detected:
             ex = (nx / 2) - bx
             ey = by - (ny / 2)
-            changes = [proportionalConstant * ex, proportionalConstant * ey]
-        else :
+            changes = [head_proportional_constant * ex, head_proportional_constant * ey]
+        else:
+            print("No ball detected...")
             changes = [np.sign(ex) * 0.1, max(0, np.random.random() - 0.5)]
-        motionProxy.changeAngles(names, changes, fractionMaxSpeed)
-        
-        # tends to align body with head
-        yaw_head, _ = motionProxy.getAngles(names, True)
-        theta = max(min(yaw_head, 1), -1)
-        x, y = 0, 0
-        freq = 1.
-        motionProxy.moveToward(x, y, theta, [["Frequency", freq]])
+        motionProxy.changeAngles(names, changes, head_fraction_max_speed)
 
         dt = dt_loop - (time.time() - t0_loop)
         if dt > 0:
